ad0339d5fe0c14579e826cfc593bbbe4d7d69e85
Thomas <ts.fremaux@gmail.com>
2023-09-26 16:51:36 +0200

    continuation de l'exercice apiPoke

---
+++ Spring/Demo_Spring_Security_Session/src/main/java/com/example/demo_spring_security_session/controllers/BaseRestController.java
@@ -0,0 +1,2 @@
+package com.example.demo_spring_security_session.controllers;public class BaseRestController {
+}
--- Spring/ExercicePokeAPI/src/main/java/com/example/exercicepokeapi/services/PokemonService.java
+++ Spring/ExercicePokeAPI/src/main/java/com/example/exercicepokeapi/services/PokemonService.java
@@ -17,8 +17,10 @@ public class PokemonService {
         RestTemplate restTemplate = restTemplateBuilder.build();
 
         ResponseEntity<PokeAPIResponse> response = restTemplate.getForEntity("/pokemon/" + value, PokeAPIResponse.class);
+        PokeAPIResponse responseDTO = response.getBody();
         String formattedId = String.format("%03d", response.getBody().getId());
-        response.getBody().setId(Integer.parseInt(formattedId));
-        return response.getBody();
+        responseDTO.setFormattedId(formattedId);
+
+        return responseDTO;
     }
 }
